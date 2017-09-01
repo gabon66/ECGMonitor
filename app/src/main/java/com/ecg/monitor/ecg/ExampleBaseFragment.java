3df2543ec6acb2f01b799899c86a12683763a832
gabon66 <gabriel.adrian.felipe@gmail.com>
2017-09-01 17:26:20 -0300

    fix test

---
+++ app/src/main/java/com/ecg/monitor/ecg/ExampleBaseFragment.java
@@ -0,0 +1,63 @@
+//******************************************************************************
+// SCICHART® Copyright SciChart Ltd. 2011-2017. All rights reserved.
+//
+// Web: http://www.scichart.com
+// Support: support@scichart.com
+// Sales:   sales@scichart.com
+//
+// ExampleBaseFragment.java is part of the SCICHART® Examples. Permission is hereby granted
+// to modify, create derivative works, distribute and publish any part of this source
+// code whether for commercial, private or personal use.
+//
+// The SCICHART® examples are distributed in the hope that they will be useful, but
+// without any warranty. It is provided "AS IS" without warranty of any kind, either
+// expressed or implied.
+//******************************************************************************
+
+package com.ecg.monitor.ecg;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.scichart.extensions.builders.SciChartBuilder;
+
+import java.util.Collections;
+import java.util.List;
+
+
+import  com.ecg.monitor.ecg.utils.widgetgeneration.Widget;
+
+import butterknife.ButterKnife;
+
+public abstract class ExampleBaseFragment extends Fragment {
+
+    protected final SciChartBuilder sciChartBuilder = SciChartBuilder.instance();
+
+    public boolean showDefaultModifiersInToolbar() {
+        return true;
+    }
+
+    public List<Widget> getToolbarItems() {
+        return Collections.emptyList();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(getLayoutId(), null);
+        ButterKnife.bind(this, root);
+        initExample();
+
+        return root;
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutId();
+
+    protected abstract void initExample();
+}
